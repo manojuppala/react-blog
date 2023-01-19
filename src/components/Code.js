704d31e3184a9c72b010758747ff7a795e32f685
manoj uppala <uppala0863@gmail.com>
2023-01-19 22:37:30 +0530

    added copy to clipboard button to code

--- src/components/Code.js
+++ src/components/Code.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Code = ({ snippet, lang }) => {
+  const [btnText, setBtnText] = useState("Copy");
+  const copytext = (text) => {
+    setBtnText("Copied!");
+    navigator.clipboard.writeText(text);
+    setTimeout(() => {
+      setBtnText("Copy");
+    }, 3000);
+  };
+
   return (
-    <pre className="prettyprint">
-      <code className={lang ? `language-${lang}` : ``}>{snippet}</code>
-    </pre>
+    <div className="pre-wrapper">
+      <pre className="prettyprint">
+        <code className={lang ? `language-${lang}` : ``}>{snippet}</code>
+      </pre>
+      <button className="copy-snippet" onClick={() => copytext(snippet)}>
+        {btnText === "Copy" ? <i className="fa fa-clone"></i> : null} {btnText}
+      </button>
+    </div>
   );
 };
 
--- src/utils/main.js
+++
@@ -1,4 +0,0 @@
-$(".anchor-div").click(function () {
-  window.location = $(this).find("a").attr("href");
-  return false;
-});
